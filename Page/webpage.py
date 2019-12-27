3e79b165931f053484bdf2a23c8a095a43b45b3c
侯伟轩 <wxhou@yunjinginc.com>
2019-12-27 17:36:19 +0800

    update: selenium基类

--- Page/webpage.py
+++ Page/webpage.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 # coding=utf-8
-import sys
-
-sys.path.append('.')
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.touch_actions import TouchActions
@@ -27,51 +24,41 @@ def sleep(seconds=1):
     :return 有些提示框不强制等待，只用显式等待会导致执行报错
     '''
     time.sleep(seconds)
-    log.info("等待%s秒！" % seconds)
+    log.info("\t等待%s秒！" % seconds)
+
+
+LOCATE_MODE = {
+    'css': By.CSS_SELECTOR,
+    'xpath': By.XPATH,
+    'name': By.NAME,
+    'id': By.ID,
+    'class': By.CLASS_NAME
+}  # 元素定位的类型
 
 
 class WebPage:
     """selenium基类"""
 
     def __init__(self, driver):
-        self.locate_mode = {
-            'css': By.CSS_SELECTOR,
-            'xpath': By.XPATH,
-            'name': By.NAME,
-            'id': By.ID
-        }  # 元素定位的类型
-
         # self.driver = webdriver.Chrome()
         self.driver = driver
         self.timeout = 10
+        self.visible = 3
         self.wait = WebDriverWait(self.driver, self.timeout)
         self.action = ActionChains(self.driver)
         self.touch = TouchActions(self.driver)
 
     def function(self, func, locator, number=None):
         """共用方法"""
-        if "==" not in locator:
-            raise AttributeError("Element does not specify a type！")
-        pattern, value = locator.split('==')
-        if pattern in self.locate_mode:
-            message = value % number if number else value
-            try:
-                element = func(self.locate_mode[pattern], message)
-            except InvalidElementStateException:
-                log.exception("元素{}，清除输入框内容失败，用户不可编辑".format(message))
-                return
-            except NoSuchElementException:
-                log.exception('当前页面没有找到元素{}'.format(message))
-                return
-            except TimeoutException:
-                log.exception('查找元素{}超时'.format(message))
-                return
-            except Exception as e:
-                raise format(e)
+        if "==" in locator:
+            pattern, value = locator.split('==')
+            if pattern in LOCATE_MODE:
+                message = value % number if number else value
+                return func(LOCATE_MODE[pattern], message)
             else:
-                return element
+                raise AttributeError('Element Type is ERROR!')
         else:
-            raise AttributeError('Element Type is ERROR!')
+            raise AttributeError("Element does not specify a type！")
 
     def get_url(self, url):
         '''打开网址并验证'''
@@ -82,12 +69,9 @@ class WebPage:
             self.driver.implicitly_wait(10)
             log.info("打开网页：%s" % url)
         except TimeoutException:
-            raise ("打开%s超时请检查网络或网址服务器" % url)
-        assert EC.url_contains(url)(
-            self.driver), "地址包含关系不正确，应为%s，实为%s" % (url,
-                                                   self.driver.current_url)
+            raise TimeoutException("打开%s超时,请检查网络或网址服务器" % url)
 
-    def Assert_title(self, text):
+    def assert_title(self, text):
         """验证网页的title文字"""
         title1 = EC.title_is(text)
         title2 = self.driver.title
@@ -95,50 +79,49 @@ class WebPage:
 
     def findelement(self, locator, number=None):
         """寻找单个元素"""
-        function = lambda *args: self.wait.until(lambda x: x.find_element(*args
-                                                                          ))
+        function = lambda *args: self.wait.until(lambda x: x.find_element(*args))
         return self.function(function, locator, number)
 
     def findelements(self, locator, number=None):
         '''查找多个相同的元素'''
-        function = lambda *args: self.wait.until(lambda x: x.find_elements(
-            *args))
+        function = lambda *args: self.wait.until(lambda x: x.find_elements(*args))
         return self.function(function, locator, number)
 
     def Exists(self, locator, number=None):  # 判断元素是否在DOM中
-        '''元素是否存在(sample)'''
-        pattern, value = locator.split('==')
-        message = value % number if number else value
-        log.info("检查元素{}在DOM中是否可见".format(message))
+        '''元素是否存在(DOM)'''
+        function = lambda *args: EC.presence_of_element_located(args)(self.driver)
         try:
-            self.driver.find_element(self.locate_mode[pattern], message)
+            self.function(function, locator, number=number)
             return True
         except:
             return False
 
-    def isElementExists(self, locator, number=None):
+    def isElementVisible(self, locator, number=None):
         '''元素是否可见'''
-        function = lambda *args: self.wait.until(
+        function = lambda *args: WebDriverWait(self.driver, self.visible).until(
             EC.visibility_of_element_located(args))
-        return self.function(function, locator, number)
+        try:
+            self.function(function, locator, number)
+            return True
+        except:
+            return False
 
     def focus(self, locator, number=None):  # 该函数的编写来源于robot-framework-selenium
         """聚焦元素"""
         ele = self.findelement(locator, number)
         self.driver.execute_script("arguments[0].focus();", ele)
-        log.info("======正在聚焦元素%s！======" % (locator % number if number else locator))
+        log.info("======元素不可见，正在聚焦元素%s！======" % (locator % number if number else locator))
 
-    def inline_scroll_bar(self,element=None,func='Left',number='10000'):
+    def inline_scroll_bar(self, element=None, func='Left', number='10000'):
         """
         内嵌滚动条（默认为向右）
-        :param types: ['ById','ByClass']
         :param element: value
         :param func: ['Left','Top']
         :param number: ['10000','0']
         """
-        js1 = 'document.getElementsByClassName("%s")[0].scroll%s=%s'%(element,func,number)
+        js1 = 'document.getElementsByClassName("%s")[0].scroll%s=%s' % (element, func, number)
         self.driver.execute_script(js1)
-        
+
     def isElementNum(self, locator):  # 获取相同元素的个数
         '''获取相同元素的个数'''
         number = len(self.findelements(locator))
@@ -148,10 +131,10 @@ class WebPage:
     def is_clear(self, locator, number=None):
         '''清空输入框'''
         self.findelement(locator, number).clear()
-        log.info("清空输入框：%s"%(locator % number if number else locator))
-        self.driver.implicitly_wait(0.5)
+        log.info("清空输入框：%s" % (locator % number if number else locator))
+        self.driver.implicitly_wait(1)
 
-    def input_text(self, locator, number=None, text=None):
+    def input_text(self, locator, text, number=None):
         '''输入(输入前先清空)'''
         self.is_clear(locator, number)
         self.findelement(locator, number).send_keys(text)
@@ -161,18 +144,17 @@ class WebPage:
         '''点击'''
         function = lambda *args: self.wait.until(
             EC.element_to_be_clickable(args))
-        ele = self.function(function, locator, number)
-        ele.click()
+        self.function(function, locator, number).click()
         log.info("点击元素%s" % (locator % number if number else locator))
         sleep()
 
     def isElementText(self, locator, number=None):
         '''获取当前的text'''
-        _text = self.findelement(locator, number).text
-        log.info("获取元素%s文字：[%s]" % ((locator % number if number else locator), _text))
-        return _text
+        __text = self.findelement(locator, number).text
+        log.info("获取元素%s文字：[%s]" % ((locator % number if number else locator), __text))
+        return __text
 
-    def textInElement(self, locator, number=None, text=None):
+    def textInElement(self, locator, text, number=None):
         '''检查某段文本在输入框中'''
         function = lambda *args: EC.text_to_be_present_in_element(args, text)(
             self.driver)
@@ -183,21 +165,19 @@ class WebPage:
         '''判断是否选中'''
         function = lambda *args: self.wait.until(
             EC.element_located_selection_state_to_be(args, True))
-        log.info("检查元素:%s 是否被选中"%(locator % number if number else locator))
+        log.info("检查元素:%s 是否被选中" % (locator % number if number else locator))
         return self.function(function, locator, number)
 
     def action_click(self, locator, number=None):
         '''使用鼠标点击'''
-        sleep()
         element = self.findelement(locator, number)
         self.driver.implicitly_wait(1)
         self.action.click(element).perform()
-        log.info("使用鼠标点击：%s"%(locator % number if number else locator))
+        log.info("使用鼠标点击：%s" % (locator % number if number else locator))
         sleep()
 
-    def action_sendkeys(self, locator, number=None, text=None):
+    def action_sendkeys(self, locator, text, number=None):
         '''action的输入方法'''
-        sleep()
         element = self.findelement(locator, number)
         sleep()
         self.is_click(locator, number)
@@ -206,13 +186,13 @@ class WebPage:
         log.info("使用鼠标方法输入：%s" % text)
         self.action._actions.pop()  # 防止重复输入
 
-    def upload_File(self, locator, number=None, filepath=None):
+    def upload_File(self, locator, filepath, number=None):
         '''上传文件'''
         self.findelement(locator, number).send_keys(filepath)
         log.info("正在上传文件：%s" % filepath)
         sleep(5)
 
-    def screenshots_of_element(self, locator, number=None, path=None):
+    def screenshots_of_element(self, locator, path, number=None):
         '''对某个元素进行截图,并返回截图路径'''
         self.focus(locator, number)  # 元素不可见则聚焦
         ele = self.findelement(locator, number)
@@ -233,15 +213,11 @@ class WebPage:
 
     def alertTextExists(self):
         "判断弹框是否出现，并返回弹框的文字"
-        try:
-            alert = EC.alert_is_present()(self.driver)
-            text = alert.text
-            log.info("Alert弹窗提示为：%s" % text)
-        except Exception as e:
-            raise e
-        else:
-            alert.accept()
-            return text
+        alert = EC.alert_is_present()(self.driver)
+        text = alert.text
+        log.info("Alert弹窗提示为：%s" % text)
+        alert.accept()
+        return text
 
     def switchToFrame(self, locator, number=None):
         """切换iframe"""
