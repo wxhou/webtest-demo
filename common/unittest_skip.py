101330403ec6e88126dd574eddab680b18a0a7b8
侯伟轩 <wxhou@yunjinginc.com>
2020-01-19 10:31:32 +0800

    add: unittest_skip file

---
+++ common/unittest_skip.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+'''
+@File : unittest_skip.py
+@Time : 2020-01-19 10:30:39
+@Author : wxhou 
+@Version : 1.0
+@Contact : 1084502012@qq.com
+'''
+import sys
+sys.path.append('.')
+import unittest
+from functools import wraps
+
+
+def skip_depend(depend=""):
+    """
+    :param depend: 依赖的用例函数名，默认为空
+    :return: wraper_func
+    """
+    def wraper_func(test_func):
+        @wraps(test_func)  # @wraps：避免被装饰函数自身的信息丢失
+        def inner_func(self):
+            if depend == test_func.__name__:
+                raise ValueError("{} cannot depend on itself".format(depend))
+            # print("self._outcome", self._outcome.__dict__)
+            # 此方法适用于python3.4 +
+            # 如果是低版本的python3，请将self._outcome.result修改为self._outcomeForDoCleanups
+            # 如果你是python2版本，请将self._outcome.result修改为self._resultForDoCleanups
+            failures = str([fail[0] for fail in self._outcome.result.failures])
+            errors = str([error[0] for error in self._outcome.result.errors])
+            skipped = str([error[0] for error in self._outcome.result.skipped])
+            flag = (depend in failures) or (depend in errors) or (
+                depend in skipped)
+            if failures.find(depend) != -1:
+                # 输出结果 [<__main__.TestDemo testMethod=test_login>]
+                # 如果依赖的用例名在failures中，则判定为失败，以下两种情况同理
+                # find()方法：查找子字符串，若找到返回从0开始的下标值，若找不到返回 - 1
+                test = unittest.skipIf(flag,
+                                       "{} failed".format(depend))(test_func)
+            elif errors.find(depend) != -1:
+                test = unittest.skipIf(flag,
+                                       "{} error".format(depend))(test_func)
+            elif skipped.find(depend) != -1:
+                test = unittest.skipIf(flag,
+                                       "{} skipped".format(depend))(test_func)
+            else:
+                test = test_func
+            return test(self)
+
+        return inner_func
+
+    return wraper_func
+
+
+if __name__ == "__main__":
+    pass
