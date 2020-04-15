1bb2cba494c8a0ed252912372730fb28759d4505
侯伟轩 <wxhou@yunjinginc.com>
2020-04-15 18:48:16 +0800

    add image_contrast_result function

--- utils/images.py
+++ utils/images.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
+import math
 import re
 import os
 import conf
+import operator
 from PIL import Image
 from utils.times import sleep
 from utils.logger import log
+from functools import reduce
 
 
 def element_screenshot(locator, path):
@@ -33,5 +36,19 @@ def get_airtest_image(name):
     return os.path.join(_path, "{}.png".format(name))
 
 
+def image_contrast_result(img1path, img2path, threshold=0.7):
+    """图像对比结果"""
+    image1 = Image.open(img1path)
+    image2 = Image.open(img2path)
+
+    h1 = image1.histogram()
+    h2 = image2.histogram()
+
+    result = math.sqrt(reduce(operator.add, list(map(lambda a, b: (a - b) ** 2, h1, h2))) / len(h1))
+    # result完全相等是0.0，设置阈值为0.7
+    return result < threshold
+
+
 if __name__ == '__main__':
-    pass
+    print(image_contrast_result('/Users/hoou/PycharmProjects/webtest-demo/airtest_image/CMS管理.png',
+                                '/Users/hoou/PycharmProjects/webtest-demo/airtest_image/头像.png', ))
